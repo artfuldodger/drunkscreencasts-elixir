0902985d9e7233b256ef247c7a5036d7ae184aee
Jon Evans <jle@foraker.com>
2016-01-05 22:16:12 -0700

    Add comeonin to prepare for authentication

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule Drunkscreencasts.Mixfile do
   def application do
     [mod: {Drunkscreencasts, []},
      applications: [:phoenix, :phoenix_html, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :postgrex]]
+                    :phoenix_ecto, :postgrex, :comeonin]]
   end
 
   # Specifies which paths to compile per environment.
@@ -36,7 +36,8 @@ defmodule Drunkscreencasts.Mixfile do
      {:phoenix_html, "~> 2.3"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.9"},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:comeonin, "~> 1.2"}]
   end
 
   # Aliases are shortcut or tasks specific to the current project.
