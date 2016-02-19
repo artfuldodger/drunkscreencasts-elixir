a04b572cdf465cf06df0b531b4ec00b32bf2b7dd
Jon Evans <jle@foraker.com>
2016-02-19 09:40:54 -0700

    Generate Video resource

--- web/router.ex
+++ web/router.ex
@@ -19,6 +19,7 @@ defmodule Drunkscreencasts.Router do
 
     resources "/users", UserController, only: [:index, :show, :new, :create]
     resources "/sessions", SessionController, only: [:new, :create, :delete]
+    resources "/videos", VideoController
 
     get "/", PageController, :index
   end
---
+++ web/views/video_view.ex
@@ -0,0 +1,3 @@
+defmodule Drunkscreencasts.VideoView do
+  use Drunkscreencasts.Web, :view
+end
